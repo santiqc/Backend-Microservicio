bde5b74ff70e0ea9ad0476c02a0469137cd37918
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-03-18 18:04:00 -0500

    Inicializando el proyecto

---
+++ src/main/java/com/backend/clienteinfoservice/ClienteInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.clienteinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteInfoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteInfoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/clienteinfoservice/ClienteInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.clienteinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
