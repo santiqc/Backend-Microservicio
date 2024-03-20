237fe5eee51f61eedb6eece797825f88fb088d0f
Santiago Quintero <86493587+santiqc@users.noreply.github.com>
2024-03-20 09:31:56 -0500

    Update Mensaje.java

--- src/main/java/com/backend/clienteinfoservice/utils/Mensaje.java
+++ src/main/java/com/backend/clienteinfoservice/utils/Mensaje.java
@@ -2,7 +2,7 @@ package com.backend.clienteinfoservice.utils;
 
 public class Mensaje {
 
-    public static final String USUARIO_NO_ENCONTRADO = "Usuario no encontrado con el tipo de documento: %s, y número de documento: %s";
+    public static final String USUARIO_NO_ENCONTRADO = "Cliente no encontrado con el tipo de documento: %s, y número de documento: %s";
     public static final String PASAPORTE = "Pasaporte";
     public static final String CEDULA_CIUDADANIA = "Cédula de Ciudadanía";
 
